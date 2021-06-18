68c3e291ce341792519a3afc2de5191627615f1f
Janis <jcostadelli@gmail.com>
2021-06-17 21:57:54 -0300

    HomePage

---
+++ src/Pages/Home/HomePage.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  MainContainer,
+  Header,
+  Footer,
+  Hero,
+  OutlinedButton,
+  Icon,
+  ButtonGroup,
+  IconGroup,
+  DownArrow,
+  Title,
+  SubTitle
+} from './styled.js';
+// amo meu mozin confusa
+
+const HomePage = () => {
+  return (
+    <MainContainer>
+
+      <Header>
+        <IconGroup>
+          <Icon href='https://www.linkedin.com/in/janiscostadelli' target='_blank'><img src='./icons/linkedin 1.png'/></Icon>
+          <Icon href='https://github.com/janiscostadelli' target='_blank'><img src='./icons/github 1.png'/></Icon>
+        </IconGroup>
+        <ButtonGroup>
+          <OutlinedButton href='#'>projects</OutlinedButton>
+          <OutlinedButton href='#'>contact</OutlinedButton>
+        </ButtonGroup>
+      </Header>
+
+      <Hero>
+        <Title>JANIS COSTADELLI</Title>
+        <img src='./icons/line.png' />
+        <SubTitle>FullStack Developer</SubTitle>
+      </Hero>
+
+      <Footer>
+        <DownArrow href='#'><img src='./icons/downArrow.png'/></DownArrow>
+      </Footer>
+    </MainContainer>
+
+  );
+}
+
+export default HomePage;
---
+++ src/Pages/Home/styled.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+background: #918BDA;
+background-image: linear-gradient(rgba(17, 68, 146, 0.2), #918BDA);
+height: 100vh;
+width: 100vw;
+font-family: 'Oswald';
+color: white;
+display: flex;
+flex-direction: column;
+`
+
+export const Title = styled.h1`
+font-size: 70px;
+`
+
+export const SubTitle = styled.h3`
+font-size: 30px;
+font-weight: 900;
+`
+
+export const Header = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 40px 100px;
+`
+
+export const Hero = styled.div`
+gap: 10px;
+flex: 1;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+`
+
+export const Footer = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+padding: 10px;
+height:80px;
+padding: 15px;
+`
+
+export const OutlinedButton = styled.a`
+color: white;
+font-size: 20px;
+padding: 8px 15px;
+border: 1px solid white;
+border-radius:5px;
+transition: 250ms;
+&:hover{
+  box-shadow: 0px 0px 5px white;
+}
+`
+
+export const DownArrow = styled.a`
+img{
+  height: 40px;
+  transition: 250ms;
+  &:hover{
+    height: 50px;
+  }
+}
+
+`
+
+export const Icon = styled.a`
+img{
+  height: 40px;
+  transition: 250ms;
+  &:hover{
+    height: 50px;
+  }
+}
+`
+
+export const ButtonGroup = styled.div`
+display: flex;
+gap: 15px;
+`
+
+export const IconGroup = styled.a`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 50px;
+width: 110px;
+gap: 10px;
+`
